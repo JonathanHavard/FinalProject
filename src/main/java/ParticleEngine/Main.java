5632fccd89ecb887af319982b517998c6fb9f0ba
JonathanHavard <jhavard@smu.edu>
2024-09-10 17:38:21 -0500

    First commit

---
+++ src/main/java/ParticleEngine/Main.java
@@ -0,0 +1,28 @@
+/*
+* Coder: Jonathan Havard
+* Date Sep 2024
+* Description: Sngine for particles
+*/
+package ParticleEngine;
+
+import processing.core.*;
+
+public class Main extends PApplet{
+    public static void main(String[] args) {
+        PApplet.main("ParticleEngine.Main");
+    }
+
+    public void settings(){
+        //Changes the size of the screen
+        size(1000, 1000);
+    }
+    public void setup(){
+        //changes the Background color
+        background(0, 0, 0);
+    }
+
+    public void draw(){
+        //Drawing function
+
+    }
+}
