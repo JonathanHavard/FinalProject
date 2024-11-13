41a80c2dbf5673be38896ff06d29120985f4a22c
JonathanHavard <jhavard@smu.edu>
2024-11-13 13:55:06 -0600

    Weird error

--- src/main/java/GraphicalUserInterface/Icon.java
+++ src/main/java/GraphicalUserInterface/Icon.java
@@ -2,7 +2,7 @@ package GraphicalUserInterface;
 
 import processing.core.*;
 
-public class Icon{
+public class Icon{//TODO ADD PREV ICO
     PApplet main; //the main class -- provides all the functionality of processing
 
     float x; //location of Icon
@@ -20,28 +20,71 @@ public class Icon{
     }
 }
     class IcoNode{
-
-        IcoNode nextIco;
+        IcoNode upIco;
+        IcoNode downIco;
+        IcoNode leftIco;
+        IcoNode rightIco;
         String icoName;
         int index;
-        IcoNode(String icoName) {
-            this.icoName = icoName;
-        }
-            
-        String getIcoName(){ //Returns foodName. Useless.
-            return icoName;
-        }
+                IcoNode(String icoName, IcoNode up, IcoNode down, IcoNode left, IcoNode right) {
+                    this.icoName = icoName;
+                }
+                String getIcoName(){ //Returns icoName. Useless.
+                    return icoName;
+                }
+                public void setIcoName(String name){ //Sets icoName. Useless.
+                    icoName = name;
+                }
+                IcoNode getUpIco(){ //Returns next Ico. Useless.
+                    return upIco;
+                }
+                IcoNode getDownIco(){ //Returns next Ico. Useless.
+                    return downIco;
+                }
+                IcoNode getLeftIco(){ //Returns next Ico. Useless.
+                    return leftIco;
+                }
+                IcoNode getRightIco(){ //Returns next Ico. Useless.
+                    return rightIco;
+                }
+                void setUpIco(IcoNode up){ //Returns next Ico. Useless.
+                     upIco = up;
+                }
+                void setDownIco(IcoNode down){ //Returns next Ico. Useless.
+                    downIco = down;
+                }
+                void setLeftIco(IcoNode left){ //Returns next Ico. Useless.
+                    leftIco = left;
+                }
+                void setRightIco(IcoNode right){ //Returns next Ico. Useless.
+                    rightIco = right;
+                }
+            }
         
-        public void setIcoName(String name){ //Sets foodName. Useless.
-            icoName = name;
-        }
-        
-        IcoNode getNextIco(){ //Returns next Food. Useless.
-            return nextIco;
-        }
-        
-        public void setNextFood(IcoNode nxt){ //Sets next Food. Useless.
-            nextIco = nxt;
-        }
-    }
+    // class IcoLinkedList{
+    //     int indexPointer = 0;
+    //     // public void addNode(String iconame){ //Inserts a node at the end of the linked list. 
+    //     //     new IcoNode(iconame);
+    //     // }
+    //     public void SetupMM(){
 
+    //     }
+    
+    //     }
+
+            
+            // if (currIcoNode.getNextIco() == null){
+            //     currIcoNode.setPrevIco(currIcoNode);
+            //     currIcoNode = new IcoNode(ico);
+            //     currIcoNode.setNextIco(currIcoNode);;
+    
+            //     indexPointer++;
+            //     currIcoNode.index = indexPointer;
+            //     return;
+            // }
+            // while (currIcoNode.nextIco != null){
+            //     currIcoNode = currIcoNode.nextIco;
+            // }
+            // currIcoNode.nextIco = new IcoNode(ico);
+
+    
--- src/main/java/GraphicalUserInterface/MainMenu.java
+++ src/main/java/GraphicalUserInterface/MainMenu.java
@@ -1,10 +1,13 @@
 package GraphicalUserInterface;
+import processing.core.PApplet;
 
-public class MainMenu extends Main{
-public void setup(){
-    draw();
-    
+public class MainMenu extends PApplet{
+    PApplet main;
+public void setup(){   
 }
+CircleIcon icon = new CircleIcon(420, 210, 100, 255, this);
+CircleIcon icon2 = new CircleIcon(220, 210, 100, 255, this); // LATER CREATE AN ARRAY LIST OF ICONS
+CursorCircle cursor = new CursorCircle(420, 210, 100, 255, this);
 
 public void keyPressed(){
     cursor.keyPressed();
